faf88d5d94defdbbc041828b3a8867b0bea8e6f4
Nelson Stoik <nelsonstoik@gmail.com>
2021-09-18 00:07:45 -0600

    hopeful fix?

--- device/fd_device/celery_runner.py
+++ device/fd_device/celery_runner.py
@@ -5,4 +5,4 @@ app = Celery()
 
 app.config_from_object("fd_device.settings:CeleryConfig")
 
-app.send_task("device.create", args=[{"Hello": "World"}])
+# app.send_task("device.create", args=[{"Hello": "World"}])
