f5bb94b431a039e9e988451e57405851c511c541
Nelson Stoik <nelsonstoik@gmail.com>
2021-09-30 13:23:16 -0600

    get 1wire running

---
+++ 1wire/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Starting all services"
+
+/usr/bin/owserver --foreground -c /etc/owfs.conf
+
+# Spin until we receive a SIGTERM (e.g. from `docker stop`)
+echo "all work done, go sleeping..."
+trap 'exit 143' SIGTERM # exit = 128 + 15 (SIGTERM)
+tail -f /dev/null & wait ${!}
