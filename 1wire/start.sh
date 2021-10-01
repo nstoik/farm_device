b41c749b6aa673f6ee1ef797170cbc73ceb77d99
Nelson <nelsonstoik@gmail.com>
2021-10-01 01:46:27 +0000

    update start script

--- 1wire/start.sh
+++ 1wire/start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-echo "Starting all services"
-
-/usr/bin/owserver --foreground -c /etc/owfs.conf && /usr/bin/owhttpd --foreground -c /etc/owfs.conf
+echo "Starting owserver"
+/usr/bin/owserver -c /etc/owfs.conf
+echo "Starting owhttpd"
+/usr/bin/owhttpd -c /etc/owfs.conf
 
 # Spin until we receive a SIGTERM (e.g. from `docker stop`)
-echo "all work done, go sleeping..."
+echo "all services started, sleeping forever..."
 trap 'exit 143' SIGTERM # exit = 128 + 15 (SIGTERM)
 tail -f /dev/null & wait ${!}
