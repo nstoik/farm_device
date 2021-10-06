66af3a155b229c679e47363ae057766c4eec9510
Nelson <nelsonstoik@gmail.com>
2021-10-06 05:24:39 +0000

    mock call to temperature

--- device/tests/device/test_update.py
+++ device/tests/device/test_update.py
@@ -1,4 +1,6 @@
 """Update module test."""
+import random
+
 import pytest
 
 from fd_device.device.update import get_device_info
@@ -7,13 +9,19 @@ from ..factories import DeviceFactory
 
 
 @pytest.mark.usefixtures("tables")
-def test_get_device_info():
+def test_get_device_info(mocker):
     """Test the get_device_info function."""
 
     device = DeviceFactory()
     device.interior_sensor = "sensor_1"
     device.exterior_sensor = "sensor_2"
     device.save()
+
+    mocker.patch(
+        "fd_device.device.update.temperature",
+        return_value=random.uniform(-40, 100),
+        autospec=True,
+    )
 
     update = get_device_info()
 
