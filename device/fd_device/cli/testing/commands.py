606a47de40b831778dc0a7b9fad1de055fd88b6b
Nelson <nelsonstoik@gmail.com>
2021-11-06 06:04:57 +0000

    try again

--- device/fd_device/cli/testing/commands.py
+++ device/fd_device/cli/testing/commands.py
@@ -106,6 +106,8 @@ def test(coverage, filename, function):
     def execute_tool(description, *args):
         """Execute a checking tool with its arguments."""
         my_env = os.environ.copy()
+        print(my_env)
+        call(["whereis", "pytest"])
         command_line = list(args)
         click.echo(f"{description}: {' '.join(command_line)}")
         rv = call(command_line, env=my_env)
