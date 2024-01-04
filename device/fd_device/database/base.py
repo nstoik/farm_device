47d16fcef0dffc0d61c029e627a12aecccbc3003
Nelson Stoik <nelsonstoik@gmail.com>
2024-01-03 23:07:20 -0700

    change to DeclarativeBase

--- device/fd_device/database/base.py
+++ device/fd_device/database/base.py
@@ -1,11 +1,13 @@
 """Database base configurations."""
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm import DeclarativeBase, scoped_session, sessionmaker
 
 from ..settings import get_config
 
-Base = declarative_base()
+
+class Base(DeclarativeBase):  # pylint: disable=too-few-public-methods
+    """Base class for SQLAlchemy model definitions."""
+
 
 config = get_config()  # pylint: disable=invalid-name
 engine = create_engine(config.SQLALCHEMY_DATABASE_URI)
